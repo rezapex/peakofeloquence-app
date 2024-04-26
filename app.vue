ca681f1fd040357290de51fc270ae68f8cab6eb7
Reza Jafar <rezajafar26@gmail.com>
2024-04-26 03:00:26 -0700

    app.vue changes

--- app.vue
+++ app.vue
@@ -1,29 +1,26 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
-const color = computed(() => colorMode.value === 'dark' ? '#111827' : 'white')
+const color = computed(() => (colorMode.value === "dark" ? "#111827" : "white"));
 
 useHead(() => ({
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color.value } // Ensure to use .value here
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color.value }, // Ensure to use .value here
   ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  link: [{ rel: "icon", href: "/gold-icon.png" }],
   htmlAttrs: {
-    lang: 'en'
-  }
+    lang: "en",
+  },
 }));
 
-
 useSeoMeta({
-  titleTemplate: 'peakofeloquence.org',
-  ogImage: '/landing.jpg',
-  twitterImage: '/landing.jpg',
-  twitterCard: 'summary_large_image'
-})
+  titleTemplate: "PeakofEloquence.org",
+  ogImage: "/landing.jpg",
+  twitterImage: "/landing.jpg",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
